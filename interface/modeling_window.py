5275e7f649910d66c769be04d1ed375696c1646d
Latiko <lavrav94@mail.ru>
2023-11-30 08:00:42 +1000

    update_modeling_window

--- interface/modeling_window.py
+++ interface/modeling_window.py
@@ -3,16 +3,17 @@ import cv2
 from PIL import Image
 from PIL import ImageTk
 import numpy as np
+from interface.widgets.additional_paramas import AdditionalParamasFrame
 
 class ModelCreatingWindow():
     def __init__(self, parent, title):
         self.root = CTkToplevel(parent)
-        self.root.geometry("500x300")
+        self.root.geometry("1030x525")
         self.root.resizable(False, False)
         self.root.title(title)
         self.root.grab_set()
-        # self.root.grid_rowconfigure(0, weight=1)
-        # self.root.grid_columnconfigure(1, weight=1)
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(1, weight=1)
 
         self.model_possible_functionality: dict = {
              'Задержанный единичный импульс': self.delayed_single_impulse,
@@ -28,85 +29,105 @@ class ModelCreatingWindow():
         }
         self.model_var = StringVar(value="Choose a model")
         
+        self.modeling_frame = CTkFrame(self.root, corner_radius=0)
+        self.modeling_frame.grid(row=0, column=0, sticky=NSEW)
+        self.modeling_frame.grid_rowconfigure(6, weight=1)
 
-        ##########create frame 1###########
-        self.size_input_frame = CTkFrame(self.root)
+        self.modeling_frame_label = CTkLabel(self.modeling_frame, text="Моделирование",
+                                             compound='left', font=CTkFont(size=15, weight='bold'))
+        self.modeling_frame_label.grid(row=0, column=0, pady=20)
+
+        # ##########create frame 1.1###########
+        self.size_input_frame = CTkFrame(self.modeling_frame_label, corner_radius=5, fg_color=("gray10", "gray10"))
         self.size_input_label = CTkLabel(self.size_input_frame, text="Введите размер модели")
-        self.size_input_x = CTkEntry(self.size_input_frame, placeholder_text='X', width=70)
-        self.size_input_y = CTkEntry(self.size_input_frame, placeholder_text='Y', width=70)
+        self.size_input_x = CTkEntry(self.size_input_frame, placeholder_text='X', width=50)
+        self.size_input_y = CTkEntry(self.size_input_frame, placeholder_text='Y', width=50)
 
-        #########create frame 2############
-        self.model_input_frame = CTkFrame(self.root)
-        self.model_input_label = CTkLabel(self.model_input_frame, text="Выберите функциональность модели")
-        self.model_option = CTkOptionMenu(self.model_input_frame, values=[*self.model_possible_functionality.keys()], variable=self.model_var, command=self.model_input_callback, width=350)
+        # #########create frame 1.2############
+        self.model_input_frame = CTkFrame(self.modeling_frame_label, corner_radius=5, fg_color=("gray10", "gray10"))
+        self.model_input_label = CTkLabel(self.model_input_frame, text="Выберите модель")
+        self.model_option = CTkOptionMenu(self.model_input_frame, values=[*self.model_possible_functionality.keys()], variable=self.model_var, command=self.model_input_callback, width=325)
 
 
-        #########create frame 3############
-        self.additional_input_frame = CTkFrame(self.root)
-        self.additional_input_label = CTkLabel(self.additional_input_frame, text="Дополнительные параметры")
-        self.input_label = CTkLabel(self.additional_input_frame, text=" ")
-        self.input_i = CTkEntry(self.additional_input_frame, placeholder_text='i0', width=70)
-        self.input_j = CTkEntry(self.additional_input_frame, placeholder_text='j0', width=70)
-        self.input_r = CTkEntry(self.additional_input_frame, placeholder_text='R', width=70)
-        self.input_N = CTkEntry(self.additional_input_frame, placeholder_text='кол-во клеток', width=70)
-        self.input_a = CTkEntry(self.additional_input_frame, placeholder_text='размер клетки', width=70)
+        # #########create frame 1.3############
+        self.additional_input_frame = AdditionalParamasFrame(self.modeling_frame_label)
+
+        self.additional_input_label = CTkLabel(self.modeling_frame_label, text="Дополнительные параметры", font=CTkFont(size=13, weight='bold'))
+
+        self.model_button = CTkButton(self.modeling_frame, text="Смоделировать", command=self.model_button_callback)
+
+        ###########create frame 2.1#############
+        self.preview_frame = CTkFrame(self.root, corner_radius=0, fg_color="transparent")
+        self.preview_frame.grid(row=0, column=1, sticky=NSEW)
+        self.preview_frame.grid_columnconfigure(0, weight=4)
+
+
+        self.previw_label = CTkLabel(self.preview_frame, text="Предварительный просмотр",
+                                             font=CTkFont(size=15, weight='bold'))
+        self.previw_label.grid(row=0, column=0, pady=20)
+
+        self.preview_canvas = CTkCanvas(self.preview_frame, width=350, height=350, background='gray22')
+        self.preview_canvas.grid(row=1, column=0, pady=20)
+
+        self.accept_button = CTkButton(self.preview_frame, text="Принять", command=self.accept_button_callback)
+        self.accept_button.grid(row=2, column=0, pady=10, padx=20, sticky=SE)
 
 
         self.draw_widgets()
+        
+
     def draw_widgets(self):
         
         ################# 1 блок. Размер изображения ###########################
 
-        self.size_input_frame.pack(side=TOP, fill=X, pady=10)
-        self.size_input_label.pack(anchor= NW, padx=10)
-        self.size_input_x.pack(side=LEFT, padx=10)
-        self.size_input_y.pack(side=LEFT, padx=10)
+
+        self.size_input_frame.grid(row=1, column=0, sticky=NSEW, padx=10, pady=10, ipady=5)
+        self.size_input_frame.grid_columnconfigure(0, minsize=200)
+        self.size_input_label.grid(row=0, column=0, sticky=W, padx=10, pady=10)
+        self.size_input_x.grid(row=1, column=0, sticky=W, padx=10)
+        self.size_input_y.grid(row=1, column=0, padx=10)
         
         ################ 2 блок. Вид модели ####################################
 
-        self.model_input_frame.pack(side=TOP, fill=X, pady=10)
-        self.model_input_label.pack(anchor= NW, padx=10)
-        self.model_option.pack(side=LEFT, padx=10)
+        self.model_input_frame.grid(row=2, column=0, sticky=NSEW, padx=10, pady=10, ipady=5)
+        self.model_input_label.grid(row=0, column=0, sticky=W, padx=10, pady=10)
+        self.model_option.grid(row=1, column=0, sticky=NSEW, padx=10)
+        
 
         ################ 3 блок. Дополнительные параметры ####################################
 
-        self.additional_input_frame.pack(side=TOP, fill=X, pady=10)
-        self.additional_input_label.pack(anchor=NW, padx=10)
-        self.input_label.pack(anchor=NW, padx=10)
+        self.additional_input_label.grid(row=3, column=0, sticky=W, padx=10, pady=5)
 
+        self.model_button.grid(row=5, column=0, sticky=SE, padx=10, pady=35)
 
     def model_input_callback(self, choice):
         self.model_possible_functionality[choice]()
+
+    def model_button_callback(self):
+        pass
+    
+    def accept_button_callback(self):
+        pass
     
     def delayed_single_impulse(self):
-        
-        self.input_label.configure(text="Введите координаты импульса:")
-                
-        self.input_i.pack(side=LEFT, padx=10)
-        self.input_j.pack(side=LEFT, padx=10)
+        self.additional_input_frame.forget()
+        self.additional_input_frame.grid_impulse()
     
     def delayed_single_hop(self):
-        self.input_label.configure(text="Введите координаты скачка:")
-        
-        self.input_label.pack(anchor=NW, padx=10)
-        self.input_i.pack(side=LEFT, padx=10)
-        self.input_j.pack(side=LEFT, padx=10)
-        
-    
-    def single_circle(self):
-         self.input_label.configure(text="Введите параметры круга:")
-         self.input_i.pack(side=LEFT, padx=10)
-         self.input_j.pack(side=LEFT, padx=10)
+        self.additional_input_frame.forget()
+        self.additional_input_frame.grid_hop()
 
+    def single_circle(self):
+        self.additional_input_frame.forget()
+        self.additional_input_frame.grid_circle()
 
     def single_square(self):
-        self.input_label.configure(text="Введите параметры квадрата:")
-        self.input_i.pack(side=LEFT, padx=10)
-        self.input_j.pack(side=LEFT, padx=10)
+        self.additional_input_frame.forget()
+        self.additional_input_frame.grid_square()
     
     def chessboard(self):
-        self.input_N.pack(side=LEFT, padx=10)
-        self.input_a.pack(side=LEFT, padx=10)
+        self.additional_input_frame.forget()
+        self.additional_input_frame.grid_chess()
     
     def circles_at_nodes(self):
          pass
---
+++ interface/widgets/additional_paramas.py
@@ -0,0 +1,64 @@
+from  customtkinter import *
+import cv2
+from PIL import Image
+from PIL import ImageTk
+import numpy as np
+
+
+class AdditionalParamasFrame:
+    def __init__(self, master):
+        self.master = master
+
+        self.frame = CTkFrame(self.master, corner_radius=5, fg_color=("gray10", "gray10"))
+        self.input_label = CTkLabel(self.frame, text=" ")
+        self.input_i = CTkEntry(self.frame, placeholder_text='i0', width=50)
+        self.input_j = CTkEntry(self.frame, placeholder_text='j0', width=50)
+        self.input_r = CTkEntry(self.frame, placeholder_text='R', width=70)
+        self.input_N = CTkEntry(self.frame, placeholder_text='кол-во', width=70)
+        self.input_a = CTkEntry(self.frame, placeholder_text='размер', width=70)
+        self.frame.grid(row=4, column=0, sticky=NSEW, padx=10, pady=10, ipady=5)
+        self.input_label.grid(row=0, column=0, sticky=NSEW, padx=10, pady=10)
+
+    def forget(self):
+        self.input_i.grid_forget()
+        self.input_j.grid_forget()
+        self.input_r.grid_forget()
+        self.input_N.grid_forget()
+        self.input_a.grid_forget()
+
+    def grid_impulse(self):
+        
+
+        self.input_label.configure(text="Введите координаты импульса:")
+        self.input_i.grid(row=1, column=0, sticky=W, padx=10)
+        self.input_j.grid(row=1, column=0, padx=10)
+    
+    def grid_hop(self):
+        
+        self.input_label.configure(text="Введите координаты скачка:")
+        self.input_i.grid(row=1, column=0, sticky=W, padx=10)
+        self.input_j.grid(row=1, column=0, padx=10)
+
+    def grid_circle(self):
+        
+        self.input_label.configure(text="Введите координаты и радиус круга:")
+        self.input_i.grid(row=1, column=0, sticky=W, padx=10)
+        self.input_j.grid(row=1, column=0, padx=10)
+        self.input_r.grid(row=2, column=0, sticky=W, padx=10, pady=5)
+
+    def grid_square(self):
+        
+        self.input_label.configure(text="Введите координаты и сторону квадрата:")
+        self.input_i.grid(row=1, column=0, sticky=W, padx=10)
+        self.input_j.grid(row=1, column=0, padx=10)
+        self.input_a.grid(row=2, column=0, sticky=W, padx=10, pady=5)
+        
+    def grid_chess(self):
+        
+        self.input_label.configure(text="Введите колиество клеток в ряду и сторону квадрата:")
+        self.input_N.grid(row=1, column=0, sticky=W, padx=10)
+        self.input_a.grid(row=1, column=0, padx=10)
+
+
+    
+    
