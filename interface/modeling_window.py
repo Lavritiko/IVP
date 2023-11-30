2b15f52bf00a8af75c43c4f29546ef39c3222a91
Kit <speedy.kit.8@gmail.com>
2023-11-30 16:35:11 +1000

    plot_ones_signal

--- interface/modeling_window.py
+++ interface/modeling_window.py
@@ -4,6 +4,7 @@ from PIL import Image
 from PIL import ImageTk
 import numpy as np
 from interface.widgets.additional_paramas import AdditionalParamasFrame
+from interface.picture_window import PicWindow
 
 class ModelCreatingWindow():
     def __init__(self, parent, title):
@@ -14,7 +15,27 @@ class ModelCreatingWindow():
         self.root.grab_set()
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(1, weight=1)
+        
+        self.modeling_frame         = CTkFrame(self.root, corner_radius=0)
+        self.modeling_frame_label   = CTkLabel(self.modeling_frame, text="Моделирование",
+                                            compound='left', font=CTkFont(size=15, weight='bold'))
+        self.model_type             = None
 
+
+        # ########## create Size model widget #################
+        self.size_input_frame       = CTkFrame(self.modeling_frame_label, corner_radius=5, fg_color=("gray10", "gray10"))
+        self.size_input_label       = CTkLabel(self.size_input_frame, text="Введите размер модели")
+        self.size_input_x           = CTkEntry(self.size_input_frame, placeholder_text='X', width=50)
+        self.size_input_x.insert(0, '450')
+        self.size_input_y           = CTkEntry(self.size_input_frame, placeholder_text='Y', width=50)
+        self.size_input_y.insert(0, '450')
+        
+
+
+        # ######### create model selection widget ##############
+        self.model_input_frame      = CTkFrame(self.modeling_frame_label, corner_radius=5, fg_color=("gray10", "gray10"))
+        self.model_input_label      = CTkLabel(self.model_input_frame, text="Выберите модель")
+        
         self.model_possible_functionality: dict = {
              'Задержанный единичный импульс': self.delayed_single_impulse,
              'Задержанный единичный скачок': self.delayed_single_hop,
@@ -27,87 +48,92 @@ class ModelCreatingWindow():
              'Белый шум с нормальным распределением': self.noise_normal_distribution,
              'Плоская гравитационная волна': self.flat_gravitational_wave,
         }
-        self.model_var = StringVar(value="Choose a model")
         
-        self.modeling_frame = CTkFrame(self.root, corner_radius=0)
-        self.modeling_frame.grid(row=0, column=0, sticky=NSEW)
-        self.modeling_frame.grid_rowconfigure(6, weight=1)
-
-        self.modeling_frame_label = CTkLabel(self.modeling_frame, text="Моделирование",
-                                             compound='left', font=CTkFont(size=15, weight='bold'))
-        self.modeling_frame_label.grid(row=0, column=0, pady=20)
-
-        # ##########create frame 1.1###########
-        self.size_input_frame = CTkFrame(self.modeling_frame_label, corner_radius=5, fg_color=("gray10", "gray10"))
-        self.size_input_label = CTkLabel(self.size_input_frame, text="Введите размер модели")
-        self.size_input_x = CTkEntry(self.size_input_frame, placeholder_text='X', width=50)
-        self.size_input_y = CTkEntry(self.size_input_frame, placeholder_text='Y', width=50)
-
-        # #########create frame 1.2############
-        self.model_input_frame = CTkFrame(self.modeling_frame_label, corner_radius=5, fg_color=("gray10", "gray10"))
-        self.model_input_label = CTkLabel(self.model_input_frame, text="Выберите модель")
-        self.model_option = CTkOptionMenu(self.model_input_frame, values=[*self.model_possible_functionality.keys()], variable=self.model_var, command=self.model_input_callback, width=325)
+        def model_input_callback(choice):
+            self.model_type = choice
+            self.model_possible_functionality[choice]()
+                
+        self.model_var              = StringVar(value="Choose a model")
+        self.model_option           = CTkOptionMenu(self.model_input_frame, values=[*self.model_possible_functionality.keys()], variable=self.model_var, command=model_input_callback, width=325)
 
 
-        # #########create frame 1.3############
-        self.additional_input_frame = AdditionalParamasFrame(self.modeling_frame_label)
-
+        # ######### create selection of parameters widget ############
         self.additional_input_label = CTkLabel(self.modeling_frame_label, text="Дополнительные параметры", font=CTkFont(size=13, weight='bold'))
+        self.additional_input_frame = AdditionalParamasFrame(self.modeling_frame_label)
+        
 
-        self.model_button = CTkButton(self.modeling_frame, text="Смоделировать", command=self.model_button_callback)
-
-        ###########create frame 2.1#############
-        self.preview_frame = CTkFrame(self.root, corner_radius=0, fg_color="transparent")
-        self.preview_frame.grid(row=0, column=1, sticky=NSEW)
-        self.preview_frame.grid_columnconfigure(0, weight=4)
-
-
-        self.previw_label = CTkLabel(self.preview_frame, text="Предварительный просмотр",
-                                             font=CTkFont(size=15, weight='bold'))
-        self.previw_label.grid(row=0, column=0, pady=20)
-
-        self.preview_canvas = CTkCanvas(self.preview_frame, width=350, height=350, background='gray22')
-        self.preview_canvas.grid(row=1, column=0, pady=20)
-
-        self.accept_button = CTkButton(self.preview_frame, text="Принять", command=self.accept_button_callback)
-        self.accept_button.grid(row=2, column=0, pady=10, padx=20, sticky=SE)
+        ########### create preview widget ###########################
+        self.model_button           = CTkButton(self.modeling_frame, text="Смоделировать", command=self.model_button_callback)
+        
+        self.preview_frame          = CTkFrame(self.root, corner_radius=0, fg_color="transparent")
+        self.previw_label           = CTkLabel(self.preview_frame, text="Предварительный просмотр",
+                                                font=CTkFont(size=15, weight='bold'))
+        self.preview_canvas         = CTkCanvas(self.preview_frame, width=350, height=350, background='gray22')
 
 
+        self.accept_button          = CTkButton(self.preview_frame, text="Принять", command=self.accept_button_callback)
+        
+
+        ###########################################################
         self.draw_widgets()
         
 
     def draw_widgets(self):
         
-        ################# 1 блок. Размер изображения ###########################
-
-
-        self.size_input_frame.grid(row=1, column=0, sticky=NSEW, padx=10, pady=10, ipady=5)
-        self.size_input_frame.grid_columnconfigure(0, minsize=200)
-        self.size_input_label.grid(row=0, column=0, sticky=W, padx=10, pady=10)
-        self.size_input_x.grid(row=1, column=0, sticky=W, padx=10)
-        self.size_input_y.grid(row=1, column=0, padx=10)
+        ################ 1 блок. Настройка моделирования ############################
+        self.modeling_frame.grid(       row=0, column=0, sticky=NSEW)
+        self.modeling_frame.grid_rowconfigure(6, weight=1)
+        self.modeling_frame_label.grid( row=0, column=0, pady=20)
         
-        ################ 2 блок. Вид модели ####################################
-
+        
+        ################# 1.1 блок. Размер изображения #############################
+        self.size_input_frame.grid( row=1, column=0, sticky=NSEW, padx=10, pady=10, ipady=5)
+        self.size_input_frame.grid_columnconfigure(0, minsize=200)
+        self.size_input_label.grid( row=0, column=0, sticky=W, padx=10, pady=10)
+        self.size_input_x.grid(     row=1, column=0, sticky=W, padx=10)
+        self.size_input_y.grid(     row=1, column=0, padx=10)
+        
+        
+        ################ 1.2 блок. Вид модели #######################################
         self.model_input_frame.grid(row=2, column=0, sticky=NSEW, padx=10, pady=10, ipady=5)
         self.model_input_label.grid(row=0, column=0, sticky=W, padx=10, pady=10)
-        self.model_option.grid(row=1, column=0, sticky=NSEW, padx=10)
+        self.model_option.grid(     row=1, column=0, sticky=NSEW, padx=10)
         
 
-        ################ 3 блок. Дополнительные параметры ####################################
+        ################ 1.3 блок. Дополнительные параметры ##########################
+        self.additional_input_label.grid(   row=3, column=0, sticky=W, padx=10, pady=5)
+        self.model_button.grid(             row=5, column=0, sticky=SE, padx=10, pady=35)
+        
+        
+        ############### 2 блок. Предварительный просмотр #############################
+        self.preview_frame.grid(    row=0, column=1, sticky=NSEW)
+        self.preview_frame.grid_columnconfigure(0, weight=4)
+        self.previw_label.grid(     row=0, column=0, pady=20)
+        self.preview_canvas.grid(   row=1, column=0, pady=20)
+        self.accept_button.grid(    row=2, column=0, pady=10, padx=20, sticky=SE)
 
-        self.additional_input_label.grid(row=3, column=0, sticky=W, padx=10, pady=5)
 
-        self.model_button.grid(row=5, column=0, sticky=SE, padx=10, pady=35)
-
-    def model_input_callback(self, choice):
-        self.model_possible_functionality[choice]()
 
     def model_button_callback(self):
-        pass
+        w = int(self.size_input_x.get())
+        h = int(self.size_input_y.get())
+        img = np.zeros((h,w,3), np.uint8)
+
+        if self.model_type == 'Задержанный единичный импульс':
+            x =   float(self.additional_input_frame.input_i.get()) +  w / 2
+            y = - float(self.additional_input_frame.input_j.get()) +  h / 2
+            cv2.circle(img, (int(x), int(y)), 3,(240, 10, 0), -1)
+            cv2.imwrite('model.png', img)
+            image = Image.open('model.png')
+            image = image.resize((350, 350))
+            image = ImageTk.PhotoImage(image)
+
+            self.preview_canvas.create_image(0, 0,anchor=NW, image=image)
+            self.root.mainloop()
     
     def accept_button_callback(self):
-        pass
+        image = cv2.imread('model.png')
+        PicWindow(self.root, 1024, 768, image_cv=image)
     
     def delayed_single_impulse(self):
         self.additional_input_frame.forget()
@@ -130,16 +156,16 @@ class ModelCreatingWindow():
         self.additional_input_frame.grid_chess()
     
     def circles_at_nodes(self):
-         pass
+        pass
     
     def random_circles(self):
-         pass
+        pass
     
     def noise_uniform_distribution(self):
-         pass
+        pass
     
     def noise_normal_distribution(self):
-         pass
+        pass
     
     def flat_gravitational_wave(self):
-         pass
+        pass
--- interface/widgets/additional_paramas.py
+++ interface/widgets/additional_paramas.py
@@ -10,12 +10,19 @@ class AdditionalParamasFrame:
         self.master = master
 
         self.frame = CTkFrame(self.master, corner_radius=5, fg_color=("gray10", "gray10"))
-        self.input_label = CTkLabel(self.frame, text=" ")
-        self.input_i = CTkEntry(self.frame, placeholder_text='i0', width=50)
-        self.input_j = CTkEntry(self.frame, placeholder_text='j0', width=50)
-        self.input_r = CTkEntry(self.frame, placeholder_text='R', width=70)
-        self.input_N = CTkEntry(self.frame, placeholder_text='кол-во', width=70)
-        self.input_a = CTkEntry(self.frame, placeholder_text='размер', width=70)
+        self.input_label    = CTkLabel(self.frame, text=" ")
+        
+        self.input_i        = CTkEntry(self.frame, placeholder_text='i0', width=50)
+        self.input_i.insert(0, '0')
+        self.input_j        = CTkEntry(self.frame, placeholder_text='j0', width=50)
+        self.input_j.insert(0, '0')
+        self.input_r        = CTkEntry(self.frame, placeholder_text='R', width=70)
+        self.input_r.insert(0, '0')
+        self.input_N        = CTkEntry(self.frame, placeholder_text='кол-во', width=70)
+        self.input_N.insert(0, '0')
+        self.input_a        = CTkEntry(self.frame, placeholder_text='размер', width=70)
+        self.input_a.insert(0, '0')
+        
         self.frame.grid(row=4, column=0, sticky=NSEW, padx=10, pady=10, ipady=5)
         self.input_label.grid(row=0, column=0, sticky=NSEW, padx=10, pady=10)
 
@@ -27,34 +34,31 @@ class AdditionalParamasFrame:
         self.input_a.grid_forget()
 
     def grid_impulse(self):
-        
-
         self.input_label.configure(text="Введите координаты импульса:")
         self.input_i.grid(row=1, column=0, sticky=W, padx=10)
+
         self.input_j.grid(row=1, column=0, padx=10)
+        
     
     def grid_hop(self):
-        
         self.input_label.configure(text="Введите координаты скачка:")
         self.input_i.grid(row=1, column=0, sticky=W, padx=10)
         self.input_j.grid(row=1, column=0, padx=10)
 
     def grid_circle(self):
-        
         self.input_label.configure(text="Введите координаты и радиус круга:")
         self.input_i.grid(row=1, column=0, sticky=W, padx=10)
         self.input_j.grid(row=1, column=0, padx=10)
         self.input_r.grid(row=2, column=0, sticky=W, padx=10, pady=5)
-
-    def grid_square(self):
         
+
+    def grid_square(self):       
         self.input_label.configure(text="Введите координаты и сторону квадрата:")
         self.input_i.grid(row=1, column=0, sticky=W, padx=10)
         self.input_j.grid(row=1, column=0, padx=10)
         self.input_a.grid(row=2, column=0, sticky=W, padx=10, pady=5)
         
-    def grid_chess(self):
-        
+    def grid_chess(self):  
         self.input_label.configure(text="Введите колиество клеток в ряду и сторону квадрата:")
         self.input_N.grid(row=1, column=0, sticky=W, padx=10)
         self.input_a.grid(row=1, column=0, padx=10)
